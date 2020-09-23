ecf70113981b372647d3f00e0b20bc59df7ff982
Joseph Karnafel <josephkarnafel@gmail.com>
2020-09-23 10:08:43 -0700

    add passing test for get_winner method which returns 1 if player chooses scissors and the computer chooses paper
    Co-authored-by: Ophelia Swen <ophelia.swen@gmail.com>

--- lib/RPS.rb
+++ lib/RPS.rb
@@ -1,13 +1,21 @@
 class Game
-  def initialize
+  def initialize(player, computer)
+    @player = player
+    @computer = computer
     @rock = 'rock'
     @paper = 'paper'
     @scissors = 'scissors'
   end
   
-  def get_winner(player, computer)
-    if player === computer
+  def get_winner
+    # 0 = tie; 1 = player win; 2 = computer win
+    if @player === @computer
       0
+    elsif @player === 'scissors' && @computer === 'paper'
+      1
     end
+  end
+
+  def winning_move
   end
 end
--- spec/RPS_spec.rb
+++ spec/RPS_spec.rb
@@ -3,7 +3,11 @@ require('RPS')
 
 describe('Game') do
   it('checks for a tie') do
-    test = Game.new()
-    expect(test.get_winner('rock', 'rock')).to(eq(0))
+    test = Game.new('rock', 'rock')
+    expect(test.get_winner).to(eq(0))
+  end
+  it('returns winning player for scissors and paper') do
+    test = Game.new('scissors', 'paper')
+    expect(test.get_winner).to(eq(1))
   end
 end
