2056579918ea8956942c143267f8f06de2d15e27
Joseph Karnafel <josephkarnafel@gmail.com>
2020-09-23 10:46:28 -0700

    add passing tests for output method whicuh neatly displays the result of the game
    Co-authored-by: Ophelia Swen <ophelia.swen@gmail.com>

--- lib/RPS.rb
+++ lib/RPS.rb
@@ -2,9 +2,6 @@ class Game
   def initialize(player, computer)
     @player = player
     @computer = computer
-    @rock = 'rock'
-    @paper = 'paper'
-    @scissors = 'scissors'
   end
   
   def get_winner
@@ -13,9 +10,18 @@ class Game
       0
     elsif ((@player == 'scissors') & (@computer == 'paper')) | ((@player == 'rock') & (@computer == 'scissors')) | ((@player == 'paper') & (@computer == 'rock'))
       1
+    else
+      2
     end
   end
 
-  def winning_move
+  def output
+    if self.get_winner == 0
+      "It's a tie! You both chose #{@player}!"
+    elsif self.get_winner == 1
+      "You win! Your opponent chose #{@computer}!"
+    elsif self.get_winner == 2
+      "You lose. Your opponent chose #{@computer}."
+    end
   end
 end
--- spec/RPS_spec.rb
+++ spec/RPS_spec.rb
@@ -18,4 +18,28 @@ describe('Game') do
     test = Game.new('paper', 'rock')
     expect(test.get_winner).to(eq(1))
   end
+  it('returns winning player for paper and scissors') do
+    test = Game.new('paper', 'scissors')
+    expect(test.get_winner).to(eq(2))
+  end
+  it('returns winning player for scissors and rock') do
+    test = Game.new('scissors', 'rock')
+    expect(test.get_winner).to(eq(2))
+  end
+  it('returns winning player for rock and paper') do
+    test = Game.new('rock', 'paper')
+    expect(test.get_winner).to(eq(2))
+  end
+  it('gives output that displays a game resulting in a tie') do
+    test = Game.new('paper', 'paper')
+    expect(test.output).to(eq("It's a tie! You both chose paper!"))
+  end
+  it('gives output that displays when the user wins') do
+    test = Game.new('paper', 'rock')
+    expect(test.output).to(eq("You win! Your opponent chose rock!"))
+  end
+  it('gives output that displays when the user loses') do
+    test = Game.new('rock', 'paper')
+    expect(test.output).to(eq("You lose. Your opponent chose paper."))
+  end
 end
